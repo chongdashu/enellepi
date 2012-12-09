54de448401f3d49edec3116e86ce8738e35c6d81
Chong-U Lim <lim.chong.u@gmail.com>
2012-12-09 12:10:23 -0500

    corpus search/concordance/indexing.

---
+++ culim/concordance.py
@@ -0,0 +1,14 @@
+import google_query
+import pickle
+import time
+import pprint
+import random
+import math
+import nltk
+
+from nltk.corpus import movie_reviews
+text = nltk.Text(movie_reviews.words())
+text.concordance("annoying")
+
+index = text._concordance_index
+index.offsets('annoying')
