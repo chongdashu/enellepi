86b229b338caf0656b003527d17b2ddbbf8d28fd
Chong-U Lim <lim.chong.u@gmail.com>
2012-12-10 14:52:31 -0500

    Fixed typo where key was 'info:' and not 'info' (added colon erroneously)

--- culim/create_datasets.py
+++ culim/create_datasets.py
@@ -20,7 +20,7 @@ for i in range(len(original_free_dataset)):
 	info = original_free_dataset[i]
 	phrases = original_free_phrases[i]
 
-	data = { "original_index" : i, "info:" : info, "phrases" : phrases}
+	data = { "original_index" : i, "info" : info, "phrases" : phrases}
 	output_free[i] = data
 
 for i in range(len(original_paid_dataset)):
@@ -29,7 +29,7 @@ for i in range(len(original_paid_dataset)):
 	info = original_paid_dataset[i]
 	phrases = original_paid_phrases[i]
 
-	data = { "original_index" : i, "info:" : info, "phrases" : phrases}
+	data = { "original_index" : i, "info" : info, "phrases" : phrases}
 	output_paid[i] = data
 
 pickle.dump(output_free, open('data/dataset_free.dat', 'w'))
