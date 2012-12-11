e0c0af960c1b3a64697366daad911fb7b784f61b
elminster55@gmail.com <elminster55@Pablo-Ortizs-MacBook-Pro.local>
2012-12-11 16:23:16 -0500

    changes

--- culim/results/results_analyzer.py
+++ culim/results/results_analyzer.py
@@ -3,63 +3,98 @@
 import pickle
 import sys
 
+#Get command line arguments.
 if len(sys.argv) < 2:
 	print 'usage: python <results dat file>'
 	print 'example: python results_free.dat'
 	sys.exit(0)
 
-#load results
-x = pickle.load(open(sys.argv[1]))
+POSITIVE_THRESHOLD = 2.4
 
-POSITIVE_THRESHOLD = 2.9
+#Load results from data file provided by user.
+data = pickle.load(open(sys.argv[1]))
+data100ofeach = []
+pos_count = 0
+neg_count = 0
+for i in range(len(data)):
+	user_rating = float(data[i]['rating-user'])
+	if user_rating > POSITIVE_THRESHOLD and pos_count < 100:
+		data100ofeach.append(data[i])
+		pos_count += 1
+	if user_rating < POSITIVE_THRESHOLD and neg_count < 100:
+		data100ofeach.append(data[i])
+		neg_count += 1
 
-#iterate over all results
-num_correct_tex_pols = 0
-num_correct_tit_pols = 0
-num_correct_two_pols = 0
-num_correct_so_pols = 0
-
-num_pos = 0
-num_neg = 0
-for i in range(len(x)):
-	user_rating = float(x[i]['rating-user'])
-	title_cls = x[i]['title-cls']
-	text_cls = x[i]['text-cls']
-	so = x[i]['so']
-
-	if user_rating > POSITIVE_THRESHOLD:
-		num_pos += 1
-	else:
-		num_neg += 1
+#Analyze the data on a per-rating basis
+def analyze_all_by_rating():
+	dist = get_rating_distribution()
+	analysis_struct = {}
 	
-	#determine the accuracy of predicting a review's polarity based on the title
-	if user_rating > POSITIVE_THRESHOLD and title_cls == 'pos':
-		num_correct_tit_pols += 1
-	if user_rating < POSITIVE_THRESHOLD and title_cls == 'neg':
-		num_correct_tit_pols += 1
+	#initialize analysis_struct
+	dist_keys = dist.keys()
+	for i in range(len(dist_keys)):
+		analysis_struct[dist_keys[i]] = {'num_correct_tex_pols' : 0, 'num_correct_tit_pols' : 0, 'num_correct_two_pols' : 0, 'num_correct_so_pols' : 0}
 	
-	#determine the accuracy of predicting a review's polarity based on the text
-	if user_rating > POSITIVE_THRESHOLD and text_cls == 'pos':
-		num_correct_tex_pols += 1
-	if user_rating < POSITIVE_THRESHOLD and text_cls == 'neg':
-		num_correct_tex_pols += 1
+	#fill up analysis_struct
+	for i in range(len(data)):
+		user_rating = float(data[i]['rating-user'])
+		user_rating_s = data[i]['rating-user']
 		
-	#determine how much the above accuracies overlap
-	if user_rating > POSITIVE_THRESHOLD and title_cls == 'pos' and text_cls == 'pos':
-		num_correct_two_pols += 1
-	if user_rating < POSITIVE_THRESHOLD and title_cls == 'neg' and text_cls == 'neg':
-		num_correct_two_pols += 1
+		title_cls = data[i]['title-cls']
+		text_cls = data[i]['text-cls']
+		so = data[i]['so']
 		
-	#determine polarity's accuracy with respect to the so
-	if so >= 0 and title_cls == 'pos':
-		num_correct_so_pols += 1
-	if so < 0 and title_cls == 'neg':
-		num_correct_so_pols += 1
+		#determine the accuracy of predicting a review's polarity based on the title
+		if user_rating > POSITIVE_THRESHOLD and title_cls == 'pos':
+			analysis_struct[user_rating_s]['num_correct_tit_pols'] += 1
+		if user_rating < POSITIVE_THRESHOLD and title_cls == 'neg':
+			analysis_struct[user_rating_s]['num_correct_tit_pols'] += 1
+		
+		#determine the accuracy of predicting a review's polarity based on the text
+		if user_rating > POSITIVE_THRESHOLD and text_cls == 'pos':
+			analysis_struct[user_rating_s]['num_correct_tex_pols'] += 1
+		if user_rating < POSITIVE_THRESHOLD and text_cls == 'neg':
+			analysis_struct[user_rating_s]['num_correct_tex_pols'] += 1
+		
+		#determine how much the above accuracies overlap
+		if user_rating > POSITIVE_THRESHOLD and title_cls == 'pos' and text_cls == 'pos':
+			analysis_struct[user_rating_s]['num_correct_two_pols'] += 1
+		if user_rating < POSITIVE_THRESHOLD and title_cls == 'neg' and text_cls == 'neg':
+			analysis_struct[user_rating_s]['num_correct_two_pols'] += 1
+		
+		#determine polarity's accuracy with respect to the so
+		if so >= 0 and title_cls == 'pos':
+			analysis_struct[user_rating_s]['num_correct_so_pols'] += 1
+		if so < 0 and title_cls == 'neg':
+			analysis_struct[user_rating_s]['num_correct_so_pols'] += 1
+	
+	#print results
+	print "\nBy-Rating Analysis"
+	analysis_keys = analysis_struct.keys()
+	for i in range(len(analysis_keys)):
+		print "(",analysis_keys[i],")"
+		print "# Correct Title      Polarizations: ", analysis_struct[analysis_keys[i]]['num_correct_tit_pols'], "\t", analysis_struct[analysis_keys[i]]['num_correct_tit_pols'] * 100.0 / dist[analysis_keys[i]], "%"
+		print "# Correct Text       Polarizations: ", analysis_struct[analysis_keys[i]]['num_correct_tex_pols'], "\t", analysis_struct[analysis_keys[i]]['num_correct_tex_pols'] * 100.0 / dist[analysis_keys[i]], "%"
+		print "# Correct Title/Text Polarizations: ", analysis_struct[analysis_keys[i]]['num_correct_two_pols'], "\t", analysis_struct[analysis_keys[i]]['num_correct_two_pols'] * 100.0 / dist[analysis_keys[i]], "%"
+		print "# Correct w.r.t. SO  Polarizations: ", analysis_struct[analysis_keys[i]]['num_correct_so_pols'], "\t", analysis_struct[analysis_keys[i]]['num_correct_so_pols'] * 100.0 / dist[analysis_keys[i]], "%"
+		print "\n"
 
-print "#Positive reviews: %s , #Negative reviews: %s" %(num_pos, num_neg)
-print "Polarity Accuracy of Titles :", (num_correct_tit_pols * 1.0) / len(x)
-print "Polarity Accuracy of Text   :", (num_correct_tex_pols * 1.0) / len(x)
-print ""
-print "Overlap of Above Polarities :", (num_correct_two_pols * 1.0) / len(x) 
-print ""
-print "Polarity Accuracy wrt SO    :", (num_correct_so_pols * 1.0) / len(x) 
+#Obtains the distribution for the star-ratings in the provided data
+def get_rating_distribution():
+	dist = {}
+	for i in range(len(data)):
+		user_rating = float(data[i]['rating-user'])
+		try:
+			dist[user_rating] += 1
+		except KeyError:
+			dist[user_rating] = 0
+	
+	print "Rating Distribution"
+	dist_keys = dist.keys()
+	for i in range(len(dist_keys)):
+		print dist_keys[i], "\t#=", dist[dist_keys[i]], "\t%=", dist[dist_keys[i]] * 100.0 / len(data)
+	
+	return dist
+
+if __name__ == '__main__':
+	analyze_all_by_rating()
