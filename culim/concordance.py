3ea427a10eb9b75e8e8389568120cf0f9b8e5e97
elminster55@gmail.com <elminster55@Pablo-Ortizs-MacBook-Pro.local>
2012-12-11 00:33:44 -0500

    bug fix

--- culim/concordance.py
+++ culim/concordance.py
@@ -76,7 +76,7 @@ def OrientationDist(word1, word2, ispositive):
 					#	1. 'word2' is 'i' words from 'word1' and a word in 'wb' is 'j' words from 'word1'
 					#   2. 'word2' is 'i' words from 'word1' and a word in 'wb' is '-j' words from 'word1'
 					if (( text.tokens[w1+i] == word2 and text.tokens[w1+j][:len(w)] == w ) or
-					( text.tokens[w1+i] == word2 and text.tokens[w1-j][:len(w)] == w:
+					( text.tokens[w1+i] == word2 and text.tokens[w1-j][:len(w)] == w):
 						totalcount += 1;
 						totaldist += abs(j) + i;
 						
